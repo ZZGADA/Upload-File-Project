52397408c6db53eb042b596f40a54096c9edff75
zhangzan <zhangzan@tal.com>
2024-08-18 14:00:42 +0800

    fit: 2024-08-18，项目框架的初始化搭建。基于nacos-sdk、gorm、viper，将Nacos和MySQL整合入Go项目中

---
+++ src/config/parameter.go
@@ -0,0 +1,54 @@
+package config
+
+// Config 读取application.yaml 文件的配置类
+type Config struct {
+	NacosConfig NacosConfig `yaml:"nacos" mapstructure:"nacos"`
+	//NacosBootstrapConfig NacosBootstrapConfig
+	DataBases DataBases `yaml:"databases" mapstructure:"databases"`
+	Server    Server    `yaml:"server" mapstructure:"server"`
+}
+
+// NacosConfig 结构体 配置了连接Nacos的参数
+type NacosConfig struct {
+	Dir                 Directory `yaml:"dir" mapstructure:"dir"`
+	LogLevel            string    `yaml:"logLevel" mapstructure:"logLevel"`
+	Username            string    `yaml:"username" mapstruture:"username"`
+	Password            string    `yaml:"password" mapstruture:"password"`
+	TimeoutMs           uint64    `yaml:"timeoutMs" mapstruture:"timeoutMs"`
+	IP                  string    `yaml:"ip" mapstruture:"ip"`
+	Port                uint64    `yaml:"port" mapstruture:"port"`
+	NotLoadCacheAtStart bool      `yaml:"notLoadCacheAtStart" mapstructure:"notLoadCacheAtStart"`
+	ContextPath         string    `yaml:"contextPath" mapstructure:"contextPath"`
+	Bootstrap           BootStrap `yaml:"bootstrap" mapstructure:"bootstrap"`
+}
+
+// Directory nacos 缓存和日志文件配置
+type Directory struct {
+	Log   string `yaml:"log" mapstruture:"log"`
+	Cache string `yaml:"cache" mapstruture:"cache"`
+}
+
+type BootStrap struct {
+	DataId string `yaml:"dataId" mapstruture:"dataId"`
+	Group  string `yaml:"group" mapstruture:"group"`
+}
+
+type Server struct {
+	Port int32 `yaml:"port" mapstructure:"port"`
+}
+type DataBases struct {
+	Mysql MySQLConfig `yaml:"mysql" mapstructure:"mysql"`
+}
+
+type NacosBootstrapConfig struct {
+	DataBases DataBases `yaml:"databases" mapstructure:"databases"`
+	Server    Server    `yaml:"server" mapstructure:"server"`
+}
+
+type MySQLConfig struct {
+	Username string `yaml:"username" mapstructure:"username"`
+	Password string `yaml:"password" mapstructure:"password"`
+	Ip       string `yaml:"ip" mapstructure:"ip"`
+	Port     int32  `yaml:"port" mapstructure:"port"`
+	Database string `yaml:"database" mapstructure:"database"`
+}
